f09acf799274f5a7d332f38c52c8c97b9ead5428
sokorieh <ericericsouza02@gmail.com>
2024-08-11 20:37:14 -0400

    import calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod calculator;
 fn main() {
     println!("Hello, world!");
 }
---
+++ target/debug/deps/rust_calculator-3590bf38d2cb6902.d
@@ -0,0 +1,6 @@
+C:\Users\hei\Desktop\rust-calculator\target\debug\deps\librust_calculator-3590bf38d2cb6902.rmeta: src/main.rs src\calculator.rs
+
+C:\Users\hei\Desktop\rust-calculator\target\debug\deps\rust_calculator-3590bf38d2cb6902.d: src/main.rs src\calculator.rs
+
+src/main.rs:
+src\calculator.rs:
